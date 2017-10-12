fe128a96ff5d9b27ff26af7b3b5ae890e13953fb
mailaw <mlaw534@gmail.com>
2017-10-11 23:15:28 -0400

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	    menu: null,
         direction: 'vertical',
         verticalCentered: true,
-        sectionsColor: ['#f2f2f2', 'white', '#7BAABE', 'whitesmoke', '#000'],
+        sectionsColor: ['#f2f2f2','#160F29','whitesmoke', '#F05D5E', '#000','#f2f2f2','#160F29','whitesmoke', '#F05D5E' ],
         anchors: [],
         scrollingSpeed: 700,
         easing: 'swing',
@@ -15,11 +15,11 @@ $(document).ready(function() {
             'textColor': '#000',
             'bulletsColor': '#000',
             'position': 'right',
-            'tooltips': ['section1', 'section2', 'section3', 'section4','section5','section6','section7','section8','section9']
+            'tooltips': ['home','section1', 'section2', 'section3', 'section4','section5','section6','quiz','sources']
         },
        	normalScrollElements: null,
         normalScrollElementTouchThreshold: 5,
-        touchSensitivity: 8,
+        touchSensitivity: 9,
         keyboardScrolling: true,
         sectionSelector: '.section',
         animateAnchor: false,
@@ -48,17 +48,27 @@ $('#arrow').click(function () {
 $('#quiz').quiz({
   questions: [
     {
-      'q': 'A sample question?',
+      'q': 'What part of the brain is used for comprehension of language?',
       'options': [
-        'Answer 1',
-        'Answer 2',
-        'Answer 3',
-        'Answer 4'
+        'Prefrontal Cortex',
+        'Hippocampus',
+        'Wernickers Area',
+        'Brocas Area'
       ],
-      'correctIndex': 1,
-      'correctResponse': 'Custom correct response.',
-      'incorrectResponse': 'Custom incorrect response.'
-    }
+      'correctIndex': 3,
+      'correctResponse': 'That is right! Great job.',
+      'incorrectResponse': 'Wrong :-( Try again and look at the 2nd slide.'
+    },
+      {
+        'q': 'A smaple question?',
+        'options': [
+          'Answer 1',
+          'Answer 2'
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Custom correct response.',
+        'incorrectResponse': 'Custom incorrect response.'
+      }
   ]
     
 });
@@ -81,3 +91,4 @@ $('#quiz').quiz({
     showNextQuote();
     
 })();
+
